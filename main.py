73411c0819eab9022168ed9a3260c7f2e06245d6
pruthvi <pradhanpruthviaditya@gmail.com>
2024-07-15 10:53:18 +0530

    checking for win

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-
+#Create blanks
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
 
-for position in range(word_length):
-    letter = chosen_word[position]
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-    if letter == guess:
-        display[position] = letter
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
+    print(display)
 
-print(display)
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
