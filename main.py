39950ed6ba2fef8dc79ff55163467ef5c24abad4
pruthvi <pradhanpruthviaditya@gmail.com>
2024-07-15 11:13:58 +0530

    improving user experience

--- main.py
+++ main.py
@@ -1,72 +1,19 @@
 import random
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
-end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-
+end_of_game = False
 lives = 6
 
+from hangman_art import logo
+print(logo)
+
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -76,14 +23,21 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
+    #Check if user is wrong.
     if guess not in chosen_word:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -97,4 +51,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
+    from hangman_art import stages
     print(stages[lives])
